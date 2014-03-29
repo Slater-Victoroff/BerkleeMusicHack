5a654a665f697bdfc288a0503f9616f394b323b2
Slater-Victoroff <slater.r.victoroff@gmail.com>
2014-03-28 20:35:39 -0400

    Added some smart preprocessing of the imag

--- images/image_difference.py
+++ images/image_difference.py
@@ -1,16 +1,18 @@
 import scipy.misc as sm
+from scipy import ndimage as nd
 import matplotlib as ml
 import matplotlib.pyplot as plt
 
-def image_difference(image1, image2):
+def image_difference(image1, image2, sigma=2, median=3):
 	"""
 	Outputs a numpy array that represents the elementwise difference between image1 and image2
 	"""
-	first = sm.imread(image1)
-	second = sm.imread(image2)
+	first = nd.gaussian_filter(sm.imread(image1, flatten=True), sigma=sigma)
+	second = nd.gaussian_filter(sm.imread(image2, flatten=True), sigma=sigma)
 	sizes = [min(pair) for pair in zip(first.shape, second.shape)]
 	core_slices = tuple([slice(0,size) for size in sizes])
-	return first[core_slices] - second[core_slices]
+	difference = first[core_slices] - second[core_slices]
+	return nd.median_filter(difference, median)
 
 def display_array(array, output=None):
 	"""
