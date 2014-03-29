c748f9d68aeaf41b6cb33edf874ea70d4fd6cd08
Stephanie Northway <snorthway@snorthway-Latitude-E6420.(none)>
2014-03-29 01:52:16 -0400

    copied webcam code over to movement_trail so it actually runs

--- movement_trail.py
+++ movement_trail.py
@@ -3,19 +3,39 @@
 import cv2
 import time
 
+
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
 def fading_circle(circ_args):
 	## time.sleep will be changed to 1/frame rate
 	time.sleep(0.05)
 	radius = circ_args[2]
 	cv2.circle(*circ_args)
 	if radius > 0:
-		print radius
 		circ_args[2] -= 1
 		return fading_circle(circ_args)
-	else:
-		## take this out later
-		print radius
+	## I know it doesn't look like anything right now
+	print radius
 
-## (50,50) will be changed to the value of the key point position
-circ_args = [frame,(50,50),5,(3,65,26,.5),-1]
-fading_circle(circ_args)
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    ## (50,50) will be changed to the value of the key point position
+    circ_args = [frame,(50,50),5,(3,65,26,.5),-1]
+	fading_circle(circ_args)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
+
+
+
+
